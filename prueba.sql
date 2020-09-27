1d797a6d09d6fb0ba64958a18da027d12b5485a0
Pablofuenza7 <pafu4012@gmail.com>
2020-09-26 23:21:14 -0300

    insertando datos a las entidades Parte 2 prueba

--- prueba.sql
+++ prueba.sql
@@ -1,27 +1,29 @@
-INSERT INTO clientes VALUES (1, 'juan', 'Cra 25 #30-12')
-INSERT INTO clientes VALUES (2, 'lily', 'Cra 25 #30-13')
-INSERT INTO clientes VALUES (2, 'pedro', 'Cra 25 #30-14')
-INSERT INTO clientes VALUES (2, 'Pablo', 'Cra 25 #30-16')
-INSERT INTO clientes VALUES (2, 'Rosa', 'Cra 25 #30-17')
+INSERT INTO clientes VALUES (1, 'juan', '123658970', 'calle los olivos');
+INSERT INTO clientes VALUES (2, 'lily', '113567890', 'pedro torres');
+INSERT INTO clientes VALUES (3, 'pedro', '169412480', 'julio zegers');
+INSERT INTO clientes VALUES (4, 'Pablo', '115669786', 'calle dorsal');
+INSERT INTO clientes VALUES (5, 'Rosa', '186106695', 'avenida brasil');
 
-INSERT INTO categorias VALUES (1, 'Frutas', 'Frutas Importadas')
-INSERT INTO categorias VALUES (2, 'Carnes', 'CARNES NACIONALES')
-INSERT INTO categorias VALUES (3, 'Cosmeticos', 'Avon')
+INSERT INTO categorias VALUES (1, 'Frutas', 'Frutas Importadas');
+INSERT INTO categorias VALUES (2, 'Carnes', 'carnes nacionales');
+INSERT INTO categorias VALUES (3, 'Cosmeticos', 'Avon');
 
+INSERT INTO facturas VALUES (1, 2222, 10-01-2020, 110.5, 0.18, 130.39, 1);
+INSERT INTO facturas VALUES (2, 2233, 25-01-2020, 130.8, 0.18, 154.344, 1);
+INSERT INTO facturas VALUES (3, 3333, 20-02-2020, 150.6, 0.18, 177.708, 2);
+INSERT INTO facturas VALUES (4, 3344, 13-04-2020, 120.1, 0.18, 141.718, 2);
+INSERT INTO facturas VALUES (5, 3355, 19-03-2020, 150.2, 0.18, 177.236, 2);
+INSERT INTO facturas VALUES (6, 4444, 07-02-2020, 124.3, 0.18, 146.674, 3);
+INSERT INTO facturas VALUES (7, 5555, 31-01-2020, 222.5, 0.18, 262.55, 4);
+INSERT INTO facturas VALUES (8, 5566, 21-06-2020, 234.1, 0.18, 276.238, 4);
+INSERT INTO facturas VALUES (9, 5577, 25-05-2020, 105.7, 0.18, 124.96, 4);
+INSERT INTO facturas VALUES (10, 5588, 30-04-2020, 300.9, 0.18, 355.062, 4);
 
-
-
-
-
-
-
-
-
-INSERT INTO productos VALUES (1, 'Mango', 'Mango Grande',10,1)
-INSERT INTO productos VALUES (2, 'Pera', 'Pera Verde',100,1)
-INSERT INTO productos VALUES (3, 'Patilla', 'Patilla Criolla',20,1)
-INSERT INTO productos VALUES (4, 'Cerdo', 'Cerdo Importado',30,2)
-INSERT INTO productos VALUES (5, 'Pollo', 'Pollo Purina',40,2)
-INSERT INTO productos VALUES (6, 'Pescado', 'Pescado de Mar',80,2)
-INSERT INTO productos VALUES (7, 'Labial', 'Labial rojo',65,3)
-INSERT INTO productos VALUES (8, 'Pestañina', 'Pestañina Negra',19,3)
+INSERT INTO productos VALUES (1, 'Mango', 'Mango Grande',10.1, 1);
+INSERT INTO productos VALUES (2, 'Pera', 'Pera Verde',100.1, 1);
+INSERT INTO productos VALUES (3, 'pavo', 'Pavo Criolla',20.1, 2);
+INSERT INTO productos VALUES (4, 'Cerdo', 'Cerdo Importado',30.2, 2);
+INSERT INTO productos VALUES (5, 'Pollo', 'Pollo Purina',40.2, 2);
+INSERT INTO productos VALUES (6, 'Pescado', 'Pescado de Mar',80.2, 2);
+INSERT INTO productos VALUES (7, 'Labial', 'Labial rojo',65.3, 3);
+INSERT INTO productos VALUES (8, 'Pestañina', 'Pestañina Negra',19.3, 3);

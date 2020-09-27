4757afddaa29d716e1b28ce121b3d9ca9796b422
Pablofuenza7 <pafu4012@gmail.com>
2020-09-26 22:27:13 -0300

    agregando modelo logico al repositorio

---
+++ prueba.sql
@@ -0,0 +1,27 @@
+INSERT INTO clientes VALUES (1, 'juan', 'Cra 25 #30-12')
+INSERT INTO clientes VALUES (2, 'lily', 'Cra 25 #30-13')
+INSERT INTO clientes VALUES (2, 'pedro', 'Cra 25 #30-14')
+INSERT INTO clientes VALUES (2, 'Pablo', 'Cra 25 #30-16')
+INSERT INTO clientes VALUES (2, 'Rosa', 'Cra 25 #30-17')
+
+INSERT INTO categorias VALUES (1, 'Frutas', 'Frutas Importadas')
+INSERT INTO categorias VALUES (2, 'Carnes', 'CARNES NACIONALES')
+INSERT INTO categorias VALUES (3, 'Cosmeticos', 'Avon')
+
+
+
+
+
+
+
+
+
+
+INSERT INTO productos VALUES (1, 'Mango', 'Mango Grande',10,1)
+INSERT INTO productos VALUES (2, 'Pera', 'Pera Verde',100,1)
+INSERT INTO productos VALUES (3, 'Patilla', 'Patilla Criolla',20,1)
+INSERT INTO productos VALUES (4, 'Cerdo', 'Cerdo Importado',30,2)
+INSERT INTO productos VALUES (5, 'Pollo', 'Pollo Purina',40,2)
+INSERT INTO productos VALUES (6, 'Pescado', 'Pescado de Mar',80,2)
+INSERT INTO productos VALUES (7, 'Labial', 'Labial rojo',65,3)
+INSERT INTO productos VALUES (8, 'Pestañina', 'Pestañina Negra',19,3)
